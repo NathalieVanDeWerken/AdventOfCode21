3c1e8efb35df2e6a63ea7b6e584a1a0e6ef5a62c
nathalievandewerken <N.J.A.vandeWerken@student.tudelft.nl>
2021-12-01 10:12:41 +0100

    day1

---
+++ code/day1.py
@@ -0,0 +1,20 @@
+from io import read_input_list
+
+def part1(input):
+    res = 0
+    for i in range(0, len(input) - 1):
+        if input[i] < input[i+1]:
+            res += 1
+    return res
+
+def part2(input):
+    res = 0
+    for i in range(0, len(input) - 3):
+        if(input[i] < input[i+3]):
+            res += 1
+    return res
+
+
+if __name__ == '__main__':
+    print(part1(read_input_list("day1.txt")))
+    print(part2(read_input_list("day1.txt")))
---
+++ io.py
@@ -0,0 +1,10 @@
+import os
+
+
+def read_input_list(path):
+    path = os.path.dirname(os.getcwd()) + "/input/" + path
+    f = open(path)
+    data = []
+    for line in f:
+        data.append(int(line))
+    return data
