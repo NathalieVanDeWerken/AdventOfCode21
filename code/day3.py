c5dcf420943b2dbec075d93dab11406e173b94fe
nathalievandewerken <N.J.A.vandeWerken@student.tudelft.nl>
2021-12-03 11:55:50 +0100

    Reformatted day 3 to get rid of some duplication.

--- code/day3.py
+++ code/day3.py
@@ -1,63 +1,51 @@
 from lib import *
 
 
-def part1(input):
-    res = 0
-    count = [0] * len(input[0])
-    for x in input:
-        z = 0
-        for y in x:
-            if (y == '1'):
-                count[z] += 1
-            z += 1
+def part1(data):
+    gamma = 0
+    counts = count_ones_per_column(data)
+
+    power = len(data[0]) - 2
+    for digit in counts:
+        if digit > len(data) / 2:
+            gamma += (2 ** power)
+        power -= 1
+    return (2 ** (len(data[0]) - 1) - 1 - gamma) * gamma
 
 
-    j = len(input[0]) - 2
-    for x in count:
-        if x > len(input)/2 :
-            res += (2**j)
-        j -= 1
-    return (2**(len(input[0])- 1) -1 - res) * res
-
-def part2(input):
+def part2(data):
     index = 0
-    input_copy = input.copy()
-    while(len(input_copy) > 1):
-        mostCommon = [0] * len(input[0])
-        for x in input_copy:
-            z = 0
-            for y in x:
-                if (y == '1'):
-                    mostCommon[z] += 1
-                z += 1
+    input_copy = data.copy()
+    while len(input_copy) > 1:
+        ones_per_column = count_ones_per_column(input_copy)
         input_copy = list(filter(lambda x:
-                                 ((x[index] == '1' and int(mostCommon[index]) >= len(input_copy) / 2)
-                                  or (x[index] == '0' and int(mostCommon[index]) < len(input_copy) / 2)), input_copy))
+                                 ((x[index] == '1' and ones_per_column[index] >= len(input_copy) / 2) or
+                                  (x[index] == '0' and ones_per_column[index] < len(input_copy) / 2)), input_copy))
         index += 1
     oxygen = input_copy[0]
 
     index = 0
-    input_copy = input.copy()
-    while(len(input_copy) > 1):
-        mostCommon = [0] * len(input[0])
-        for x in input_copy:
-            z = 0
-            for y in x:
-                if (y == '1'):
-                    mostCommon[z] += 1
-                z += 1
+    input_copy = data.copy()
+    while len(input_copy) > 1:
+        ones_per_column = count_ones_per_column(input_copy)
         input_copy = list(filter(lambda x:
-                                 ((x[index] == '1' and int(mostCommon[index]) < len(input_copy) / 2)
-                                  or (x[index] == '0' and int(mostCommon[index]) >= len(input_copy) / 2)), input_copy))
+                                 ((x[index] == '1' and ones_per_column[index] < len(input_copy) / 2) or
+                                  (x[index] == '0' and ones_per_column[index] >= len(input_copy) / 2)), input_copy))
         index += 1
     scrubber = input_copy[0]
 
-    return int(oxygen, 2) * int(scrubber,2)
+    return int(oxygen, 2) * int(scrubber, 2)
 
 
-
-
-
+def count_ones_per_column(array):
+    counts = [0] * len(array[0])
+    for string in array:
+        index = 0
+        for char in string:
+            if char == '1':
+                counts[index] += 1
+            index += 1
+    return counts
 
 
 if __name__ == '__main__':
