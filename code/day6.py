101be029901b1372e61c21b1f1474fd0111a46fc
nathalievandewerken <N.J.A.vandeWerken@student.tudelft.nl>
2021-12-06 08:58:23 +0100

    Reformatted day 6

--- code/day6.py
+++ code/day6.py
@@ -1,30 +1,38 @@
-from queue import PriorityQueue
-
 from lib import read_input_list_single_line
 
+
 def part1(data):
-    fish = []
-    for x in data:
-        fish.append(int(x))
-    for i in range(0,80):
-        for j,x in enumerate(fish):
-            if x == 0:
-                fish[j] = 6
-                fish.append(9)
-            else:
-                fish[j] -= 1
-    return len(fish)
+    return count_fish(data, 80)
+
 
 def part2(data):
-    lengths = [0] * 9
+    return count_fish(data, 256)
+
+
+def count_fish(data, days):
+    number_of_fish = [0] * 9
     for x in data:
-        lengths[int(x)] += 1
-    for i in range(0,256):
-        done_fish = lengths[0]
-        lengths.pop(0)
-        lengths.append(done_fish)
-        lengths[6] += done_fish
-    return sum(lengths)
+        number_of_fish[x] += 1
+    for i in range(0, days):
+        number_of_timer_done = number_of_fish[0]
+        number_of_fish.pop(0)
+        number_of_fish.append(number_of_timer_done)
+        number_of_fish[6] += number_of_timer_done
+    return sum(number_of_fish)
+
+
+def count_fish_naive(data, days):
+    fish = []
+    for x in data:
+        fish.append(x)
+    for i in range(0, days):
+        for ind, timer in enumerate(fish):
+            if timer == 0:
+                fish[ind] = 6
+                fish.append(9)
+            else:
+                fish[timer] -= 1
+    return len(fish)
 
 
 if __name__ == '__main__':
