095fd21e36f689dfb0e4727f366c5719e9073f99
nathalievandewerken <N.J.A.vandeWerken@student.tudelft.nl>
2021-12-09 23:37:31 +0100

    Cleaned up code day 9.

--- code/day09.py
+++ code/day09.py
@@ -2,7 +2,7 @@ from lib import read_input_heights
 
 
 def part1(data):
-    return sum(data[x[0]][x[1]] for x in find_all_lows(data)) + len(find_all_lows(data))
+    return sum(data[x[0]][x[1]] + 1 for x in find_all_lows(data))
 
 
 def part2(data):
@@ -14,9 +14,11 @@ def part2(data):
 
 def find_all_lows(data):
     result = []
+
     for indy, row in enumerate(data):
         for indx, number in enumerate(row):
             absolute_min = True
+
             if (indy - 1) >= 0 and data[indy - 1][indx] <= number:
                 absolute_min = False
             if (indx - 1) >= 0 and data[indy][indx - 1] <= number:
@@ -25,33 +27,27 @@ def find_all_lows(data):
                 absolute_min = False
             if (indy + 1) < len(data) and data[indy + 1][indx] <= number:
                 absolute_min = False
+
             if absolute_min:
                 result.append((indy, indx))
+
     return result
 
 
 def find_size(i, j, data):
-    result = 1
-    queue = []
-    visited = {(i, j)}
-    if i < len(data) - 1:
-        queue.append((i + 1, j))
-    if i > 0:
-        queue.append((i - 1, j))
-    if j < len(data[0]) - 1:
-        queue.append((i, j + 1))
-    if j > 0:
-        queue.append((i, j - 1))
+    result = 0
+    queue = [(i, j)]
+    visited = set()
     while queue:
-        element = queue.pop()
-        x = element[0]
-        y = element[1]
-        if element in visited:
+        x, y = queue.pop()
+
+        if (x, y) in visited:
             continue
-        visited.add(element)
+        visited.add((x, y))
         if data[x][y] == 9:
             continue
         result += 1
+
         if x > 0 and data[x][y] < data[x - 1][y]:
             queue.append((x - 1, y))
         if y > 0 and data[x][y] < data[x][y - 1]:
@@ -60,6 +56,7 @@ def find_size(i, j, data):
             queue.append((x + 1, y))
         if y < len(data[0]) - 1 and data[x][y] < data[x][y + 1]:
             queue.append((x, y + 1))
+
     return result
 
 
